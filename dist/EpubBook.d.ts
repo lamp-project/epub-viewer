46134561f24bc9bc0b14c4fc527144b16b5e7406
Stijlbreuk Generic <generic@stijlbreuk.nl>
2022-02-10 13:52:52 +0330

    build

--- dist/EpubBook.d.ts
+++ dist/EpubBook.d.ts
@@ -1,9 +1,9 @@
 import { Book } from 'epubjs';
 import { PackagingMetadataObject } from 'epubjs/types/packaging';
 export declare class EpubBook {
-    readonly metadata: PackagingMetadataObject;
-    readonly cover: Blob;
-    readonly content?: ArrayBuffer;
+    metadata: PackagingMetadataObject;
+    cover: Blob;
+    content?: ArrayBuffer;
     static extractBookCover(book: Book): Promise<Blob>;
     static extractMeta(content: ArrayBuffer): Promise<{
         cover: Blob;
