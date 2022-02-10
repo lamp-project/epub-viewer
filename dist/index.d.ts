ca00fedb7b71ac81b96940485f515f5d5acb64a2
Stijlbreuk Generic <generic@stijlbreuk.nl>
2022-02-10 14:48:26 +0330

    fix: package

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './EpubBook';
-export * from './EpubViewerBase';
-export * from './EpubViewer';
-export * from './EpubPaginator';
-export * from './PaginatedEpubViewer';
-export * from './StatefullPaginatedEpubViewer';
