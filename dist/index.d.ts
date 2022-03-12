0cae786da496549f5e0fe2d0786fb0ade013a39b
Stijlbreuk Generic <generic@stijlbreuk.nl>
2022-03-12 10:42:01 +0330

    .

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './utils/Epub';
-export * from './utils/StatefulLibrary';
-export * from './EpubViewerBase';
-export * from './EpubViewer';
-export * from './EpubPaginator';
-export * from './PaginatedEpubViewer';
-export * from './StatefulPaginatedEpubViewer';
