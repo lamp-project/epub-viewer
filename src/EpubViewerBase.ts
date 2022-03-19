1d02624a58b83bf10a5862cbe19b94245c3bfd39
Stijlbreuk Generic <generic@stijlbreuk.nl>
2022-03-19 09:10:55 +0330

    Release 1.0.0

--- src/EpubViewerBase.ts
+++ src/EpubViewerBase.ts
@@ -3,159 +3,151 @@ import Epub, { Contents, Rendition } from 'epubjs';
 import { RenditionOptions, Location } from 'epubjs/types/rendition';
 
 export interface Point2D {
-	x: number;
-	y: number;
+  x: number;
+  y: number;
 }
 
 export interface TouchEvent {
-	start: Point2D;
-	end: Point2D;
-	moved: boolean;
+  start: Point2D;
+  end: Point2D;
+  moved: boolean;
 }
 
 export interface RelocatedEventPayload {
-	location: Location;
-	chapter: string;
+  location: Location;
+  chapter: string;
 }
 
 export interface ContentEventPayload {
-	contents: Contents;
-	view: Rendition;
+  contents: Contents;
+  view: Rendition;
 }
 
 export class EpubViewerBase extends EventEmitter {
-	protected element!: Element;
-	protected rendition!: Rendition;
-	protected currentLocation!: string;
-	protected currentChapter!: string;
-	constructor(protected readonly book = Epub()) {
-		super();
-	}
+  protected element!: Element;
+  protected rendition!: Rendition;
+  protected currentLocation!: string;
+  protected currentChapter!: string;
+  constructor(protected readonly book = Epub()) {
+    super();
+  }
 
-	destroy() {
-		this.book.destroy();
-	}
+  destroy() {
+    this.book.destroy();
+  }
 
-	public get size() {
-		return {
-			width: this.element?.clientWidth,
-			height: this.element?.clientHeight,
-		};
-	}
+  public get size() {
+    return {
+      width: this.element?.clientWidth,
+      height: this.element?.clientHeight,
+    };
+  }
 
-	public async initialize(input: ArrayBuffer) {
-		await this.book.open(input, 'binary');
-		await this.book.ready;
-		await this.book.loaded.navigation;
-	}
+  public async initialize(input: ArrayBuffer) {
+    await this.book.open(input, 'binary');
+    await this.book.ready;
+    await this.book.loaded.navigation;
+  }
 
-	public display(
-		element: Element,
-		options?: RenditionOptions,
-		target?: string,
-	) {
-		this.element = element;
-		this.rendition = this.book.renderTo(element, {
-			width: '100%',
-			height: '100%',
-			...options,
-			allowScriptedContent: true,
-		});
-		this.registerEventListeners();
-		this.registerThemes();
-		return this.rendition.display(target);
-	}
+  public display(element: Element, options?: RenditionOptions, target?: string) {
+    this.element = element;
+    this.rendition = this.book.renderTo(element, {
+      width: '100%',
+      height: '100%',
+      ...options,
+      allowScriptedContent: true,
+    });
+    this.registerEventListeners();
+    this.registerThemes();
+    return this.rendition.display(target);
+  }
 
-	public goTo(target: string) {
-		console.log(target);
-		this.rendition.display(target);
-	}
+  public goTo(target: string) {
+    console.log(target);
+    this.rendition.display(target);
+  }
 
-	protected registerEventListeners() {
-		// rendition events
-		this.rendition.on('relocated', (location: Location) => {
-			this.currentLocation = location.end.cfi;
-			this.currentChapter = this.getChapter(location.start.href);
-			this.emit('relocated', {
-				location,
-				chapter: this.currentChapter,
-			} as RelocatedEventPayload);
-		});
-		this.rendition.hooks.content.register(
-			(contents: Contents, view: Rendition) => {
-				this.emit('content', contents.document);
-			},
-		);
-		this.rendition.hooks.render.register((iframeView: { document: any; }) => {
-			this.emit('render', iframeView.document);
-		});
-		// gestures events
-		this.listenRenditionGestureEvents();
-	}
+  protected registerEventListeners() {
+    // rendition events
+    this.rendition.on('relocated', (location: Location) => {
+      this.currentLocation = location.end.cfi;
+      this.currentChapter = this.getChapter(location.start.href);
+      this.emit('relocated', {
+        location,
+        chapter: this.currentChapter,
+      } as RelocatedEventPayload);
+    });
+    this.rendition.hooks.content.register((contents: Contents, view: Rendition) => {
+      this.emit('content', contents.document);
+    });
+    this.rendition.hooks.render.register((iframeView: { document: any }) => {
+      this.emit('render', iframeView.document);
+    });
+    // gestures events
+    this.listenRenditionGestureEvents();
+  }
 
-	protected getChapter(href: string) {
-		const [toc] = this.book.navigation.toc.filter((toc) =>
-			toc.href.includes(href),
-		);
-		return toc?.label.trim();
-	}
+  protected getChapter(href: string) {
+    const [toc] = this.book.navigation.toc.filter((toc) => toc.href.includes(href));
+    return toc?.label.trim();
+  }
 
-	protected registerThemes() {
-		this.rendition.themes.register('dark', {
-			body: {
-				color: 'white',
-				'font-size': 'x-large',
-				'padding-top': '0 !important',
-				'padding-bottom': '0 !important',
-			},
-		});
-		this.rendition.themes.register('light', {
-			body: {
-				color: 'black',
-				'font-size': 'x-large',
-				'padding-top': '0 !important',
-				'padding-bottom': '0 !important',
-			},
-		});
-		this.rendition.themes.select('light');
-		// this.rendition.themes.register('lamp-reader', 'themse/lamp-reader.css');
-		// this.rendition.themes.select('lamp-reader');
-	}
+  protected registerThemes() {
+    this.rendition.themes.register('dark', {
+      body: {
+        color: 'white',
+        'font-size': 'x-large',
+        'padding-top': '0 !important',
+        'padding-bottom': '0 !important',
+      },
+    });
+    this.rendition.themes.register('light', {
+      body: {
+        color: 'black',
+        'font-size': 'x-large',
+        'padding-top': '0 !important',
+        'padding-bottom': '0 !important',
+      },
+    });
+    this.rendition.themes.select('light');
+    // this.rendition.themes.register('lamp-reader', 'themse/lamp-reader.css');
+    // this.rendition.themes.select('lamp-reader');
+  }
 
-	private handleEvent(event: TouchEvent) {
-		if (!event.moved) {
-			return 'tap';
-		} else if (event.end.x < event.start.x) {
-			// swiped left!
-			return 'swipe:left';
-		} else if (event.end.x > event.start.x) {
-			// swiped right!
-			return 'swipe:right';
-		}
-	}
+  private handleEvent(event: TouchEvent) {
+    if (!event.moved) {
+      return 'tap';
+    } else if (event.end.x < event.start.x) {
+      // swiped left!
+      return 'swipe:left';
+    } else if (event.end.x > event.start.x) {
+      // swiped right!
+      return 'swipe:right';
+    }
+  }
 
-	private listenRenditionGestureEvents() {
-		const touchEvent = {
-			start: { x: 0, y: 0 },
-			end: { x: 0, y: 0 },
-			moved: false,
-		};
+  private listenRenditionGestureEvents() {
+    const touchEvent = {
+      start: { x: 0, y: 0 },
+      end: { x: 0, y: 0 },
+      moved: false,
+    };
 
-		this.rendition.on('touchstart', (event: any) => {
-			touchEvent.moved = false;
-			touchEvent.start.x = event.changedTouches[0].screenX;
-			touchEvent.start.y = event.changedTouches[0].screenY;
-		});
+    this.rendition.on('touchstart', (event: any) => {
+      touchEvent.moved = false;
+      touchEvent.start.x = event.changedTouches[0].screenX;
+      touchEvent.start.y = event.changedTouches[0].screenY;
+    });
 
-		this.rendition.on('touchmove', () => {
-			touchEvent.moved = true;
-		});
+    this.rendition.on('touchmove', () => {
+      touchEvent.moved = true;
+    });
 
-		this.rendition.on('touchend', (event: any) => {
-			touchEvent.end.x = event.changedTouches[0].screenX;
-			touchEvent.end.y = event.changedTouches[0].screenY;
-			const gesture = this.handleEvent(touchEvent);
-			this.rendition.emit(gesture);
-		});
-	}
+    this.rendition.on('touchend', (event: any) => {
+      touchEvent.end.x = event.changedTouches[0].screenX;
+      touchEvent.end.y = event.changedTouches[0].screenY;
+      const gesture = this.handleEvent(touchEvent);
+      this.rendition.emit(gesture);
+    });
+  }
 }
--- src/StatefulPaginatedEpubViewer.ts
+++ src/StatefulPaginatedEpubViewer.ts
@@ -1,76 +1,66 @@
 import { RenditionOptions } from 'epubjs/types/rendition';
-import {
-	Epub,
-	library,
-	BookInfo,
-	PaginatedEpubViewer,
-	RelocatedEventPayload,
-} from '.';
+import { Epub, library, BookInfo, PaginatedEpubViewer, RelocatedEventPayload } from '.';
 
 export class StatefulPaginatedEpubViewer extends PaginatedEpubViewer {
-	protected readonly bookInfo: BookInfo;
-	constructor(epub: Epub) {
-		super(epub.content);
-		this.bookInfo = epub.info;
-		this.on('page-changed', this.persistPagination.bind(this));
-	}
+  protected readonly bookInfo: BookInfo;
+  constructor(epub: Epub) {
+    super(epub.content);
+    this.bookInfo = epub.info;
+    this.on('page-changed', this.persistPagination.bind(this));
+  }
 
-	protected async persistPagination() {
-		await library.updateInfo(this.bookInfo);
-	}
+  protected async persistPagination() {
+    await library.updateInfo(this.bookInfo);
+  }
 
-	public async display(element: Element, options?: RenditionOptions): Promise<void> {
-		const lastLocation = this.bookInfo.pagination.currentLocation?.end.cfi;
-		await super.display(
-			element,
-			options,
-			lastLocation,
-		);
-		
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore
-		if (lastLocation && this.rendition.currentLocation().end.cfi !== lastLocation) {
-			console.warn(`Last location didn't load normal: ${lastLocation}`);
-			return this.goTo(lastLocation);
-		}
-	}
+  public async display(element: Element, options?: RenditionOptions): Promise<void> {
+    const lastLocation = this.bookInfo.pagination.currentLocation?.end.cfi;
+    await super.display(element, options, lastLocation);
 
-	/**
-	 * Skipping pagination if exists
-	 */
-	protected async paginate() {
-		if (
-			this.bookInfo.pagination.size.width === this.size.width &&
-			this.bookInfo.pagination.size.height === this.size.height
-		) {
-			this.pages = this.bookInfo.pagination.pages;
-			console.log('pagination will load from cache');
-		} else {
-			super.paginate();
-		}
-	}
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (lastLocation && this.rendition.currentLocation().end.cfi !== lastLocation) {
+      console.warn(`Last location didn't load normal: ${lastLocation}`);
+      return this.goTo(lastLocation);
+    }
+  }
 
-	protected registerEventListeners() {
-		super.registerEventListeners();
-		// rendition events
-		this.on('relocated', ({ location, chapter }: RelocatedEventPayload) => {
-			this.bookInfo.pagination.currentLocation = location;
-			this.bookInfo.pagination.currentChapter = chapter;
-			this.bookInfo.pagination.currentPage = this.currentPage;
-			this.emit('page-changed', this.bookInfo.pagination);
-		});
+  /**
+   * Skipping pagination if exists
+   */
+  protected async paginate() {
+    if (
+      this.bookInfo.pagination.size.width === this.size.width &&
+      this.bookInfo.pagination.size.height === this.size.height
+    ) {
+      this.pages = this.bookInfo.pagination.pages;
+      console.log('pagination will load from cache');
+    } else {
+      super.paginate();
+    }
+  }
 
-		// pagination events
-		this.on('pagination-update', () => {
-			this.bookInfo.pagination.pages = this.pages;
-			this.bookInfo.pagination.currentPage = this.currentPage;
-			this.emit('page-changed', this.bookInfo.pagination);
-		});
+  protected registerEventListeners() {
+    super.registerEventListeners();
+    // rendition events
+    this.on('relocated', ({ location, chapter }: RelocatedEventPayload) => {
+      this.bookInfo.pagination.currentLocation = location;
+      this.bookInfo.pagination.currentChapter = chapter;
+      this.bookInfo.pagination.currentPage = this.currentPage;
+      this.emit('page-changed', this.bookInfo.pagination);
+    });
 
-		this.on('pagination-done', () => {
-			this.bookInfo.pagination.pages = this.pages;
-			this.bookInfo.pagination.size = this.size;
-			this.emit('page-changed', this.bookInfo.pagination);
-		});
-	}
+    // pagination events
+    this.on('pagination-update', () => {
+      this.bookInfo.pagination.pages = this.pages;
+      this.bookInfo.pagination.currentPage = this.currentPage;
+      this.emit('page-changed', this.bookInfo.pagination);
+    });
+
+    this.on('pagination-done', () => {
+      this.bookInfo.pagination.pages = this.pages;
+      this.bookInfo.pagination.size = this.size;
+      this.emit('page-changed', this.bookInfo.pagination);
+    });
+  }
 }
