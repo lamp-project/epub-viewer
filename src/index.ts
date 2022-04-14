78c84070ad49fd15cdb854f067c4898540c7f1aa
Stijlbreuk Generic <generic@stijlbreuk.nl>
2022-04-14 12:49:03 +0430

    feat: StatefulEpubViewer

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './utils/Epub';
 export * from './utils/StatefulLibrary';
 export * from './EpubViewerBase';
 export * from './EpubViewer';
+export * from './StatefulEpubViewer';
 export * from './EpubPaginator';
 export * from './PaginatedEpubViewer';
 export * from './StatefulPaginatedEpubViewer';
